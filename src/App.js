cb6cb3c471322f8b3d0b4f6c74dae1e0835d6424
Weiyed <1357855389@qq.com>
2022-06-16 19:13:46 -1000

    basic react activity

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+/*import logo from './logo.svg';
 import './App.css';
 
 function App() {
@@ -22,4 +22,20 @@ function App() {
   );
 }
 
+export default App;*/
+
+import React ,{useState}from 'react';
+import TodoList from './TodoList'
+function App(){
+  const [todos, setTodos] = useState([{id: 1, name: 'Todo 1', complete: false}])
+  return(
+    <>
+     <TodoList todos={todos}/>
+     <input type= "text" />
+     <button>Add Todo</button>
+     <button>Clear Complete</button>
+     <div>0 left to do</div>
+    </>
+  )
+}
 export default App;
